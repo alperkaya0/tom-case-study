5b444613147ee591c4a93be92f50d34abfdf0496
Alper Kaya <alperkaya787856@gmail.com>
2023-05-10 16:42:12 +0300

    Add cors and grafana&prometheus

--- src/main/java/com/tom/shoppingcartapi/controller/ShoppingCartController.java
+++ src/main/java/com/tom/shoppingcartapi/controller/ShoppingCartController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/v1/shopping-carts")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class ShoppingCartController {
 	private final ShoppingCartService shoppingCartService;
 	
